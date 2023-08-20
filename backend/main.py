2a0d81a0c6dd8f76febcdd538fd1389d1e08b022
shichiji <skosei554@yahoo.co.jp>
2023-08-20 20:58:56 +0900

    DBインサート、APIからデータ取得

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import mysql.connector
+import requests
+import romkan
+import json
 
 app = FastAPI()
 
@@ -16,30 +19,165 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def Hello():
-    return {"Hello":"World!"}
+# MySQL接続情報
+db_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "xquk59",
+    "database": "weatherForecast",
+}
 
-@app.get("/weather")
-def get_weather(city: str):
+# OpenWeatherMap APIキー
+API_TOKEN = "49ffe79df3664f91a32b0ee522dcbbb2"
+
+# 都道府県のリスト
+prefectures = [
+    "Hokkaido", "Aomori", "Iwate", "Miyagi", "Akita", "Yamagata", "Fukushima",
+    "Ibaraki", "Tochigi", "Gunma", "Saitama", "Chiba", "Tokyo", "Kanagawa",
+    "Niigata", "Toyama", "Ishikawa", "Fukui", "Yamanashi", "Nagano", "Gifu",
+    "Shizuoka", "Aichi", "Mie", "Shiga", "Kyoto", "Osaka", "Hyogo", "Nara",
+    "Wakayama", "Tottori", "Shimane", "Okayama", "Hiroshima", "Yamaguchi",
+    "Tokushima", "Kagawa", "Ehime", "Kochi", "Fukuoka", "Saga", "Nagasaki",
+    "Kumamoto", "Oita", "Miyazaki", "Kagoshima", "Okinawa"
+]
+
+# DBからデータ取得
+def get_weather_from_mysql(city: str):
     try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="xquk59",
-            database="weatherForecast"
-        )
+        # MySQL接続
+        connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
-        query = "SELECT * FROM weatherForecast WHERE prefecture = %s"
+
+        # MySQLから天気情報取得
+        query = "SELECT weather,iconImage FROM weatherForecast WHERE prefecture = %s"
         cursor.execute(query, (city,))
         result = cursor.fetchall()
-
-        return {"weather_data": result}
+        return result
 
     except mysql.connector.Error as err:
-        return {"error": str(err)}
+        raise err
 
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
+
+# 天気APIからデータ取得
+def get_weather_from_api(city: str):
+    try:
+        response = requests.get(
+            "https://api.openweathermap.org/data/2.5/weather",
+            params={
+                "q": city,  # 都市名で取得
+                "appid": API_TOKEN,
+                "units": "metric",
+                "lang": "ja",
+            },
+        )
+        weather_data = response.json()
+        return weather_data
+
+    except requests.RequestException as err:
+        raise err
+
+# 天気予報を取得してDBにインサート
+def insert_weather_into_db(city: str, weather_data: dict):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        # MySQLに天気情報を挿入
+        insert_query = "INSERT INTO weatherForecast (prefecture, weather, iconImage) VALUES (%s, %s, %s)"
+        # print("weather_data", weather_data)
+        insert_values = (
+            city, weather_data['weather'][0]['description'], weather_data['weather'][0]['icon'])
+        cursor.execute(insert_query, insert_values)
+        connection.commit()
+
+    except mysql.connector.Error as err:
+        raise err
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+# 全都道府県の天気予報を取得してDBにインサート
+for city in prefectures:
+    try:
+        # 天気APIからデータ取得
+        weather_data = get_weather_from_api(city)
+
+        # データベースにインサート
+        insert_weather_into_db(city, weather_data)
+
+    except Exception as err:
+        print(f"Error processing {city}: {err}")
+
+
+@app.get("/weather")
+def get_weather(city: str):
+    try:
+        result_from_mysql = get_weather_from_mysql(city)
+        return {"result_from_mysql": result_from_mysql}
+
+    except (mysql.connector.Error, requests.RequestException) as err:
+        return {"error": str(err)}
+
+
+@app.get("/weatherAPI")
+def get_weatherAPI(city: str):
+    try:
+        result_from_api = get_weather_from_api(city)
+        insert_weather_into_db(city, result_from_api)
+        return {"weather_from_api": result_from_api}
+
+    except (mysql.connector.Error, requests.RequestException) as err:
+        return {"error": str(err)}
+
+
+# @app.get("/")
+# def Hello():
+#     return {"Hello": "World!"}
+
+
+# @app.get("/weather")
+# def get_weather(city: str):
+#     try:
+#         # MySQL接続
+#         connection = mysql.connector.connect(**db_config)
+#         cursor = connection.cursor()
+
+#         # MySQLから天気情報取得
+#         query = "SELECT weather FROM weatherForecast WHERE prefecture = %s"
+#         cursor.execute(query, (city,))
+#         result = cursor.fetchall()
+
+#         # OpenWeatherMap APIから天気情報取得
+#         response = requests.get(
+#             "https://api.openweathermap.org/data/2.5/weather",
+#             params={
+#                 "q": city,  # 都市名で取得
+#                 "appid": API_TOKEN,
+#                 "units": "metric",
+#                 "lang": "ja",
+#             },
+#         )
+
+#         weather_data = response.json()
+#         # print(weather_data.weather[0])
+
+#         # MySQLに天気情報を挿入
+#         # insert_query = "INSERT INTO weatherForecast (prefecture, weather) VALUES (%s, %s)"
+#         # insert_values = (city, json.dumps(weather_data))
+#         # cursor.execute(insert_query, insert_values)
+#         # connection.commit()
+#         return {"weather_from_mysql": result, "weather_from_api": weather_data}
+
+#     except mysql.connector.Error as err:
+#         return {"error": str(err)}
+
+#     finally:
+#         if connection.is_connected():
+#             cursor.close()
+#             connection.close()
--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = React.useState();
+  const [city, setCity] = useState("");
+  // const [city, setSelectedPrefecture] = useState("");
+  const url = "http://127.0.0.1:8000";
+
+  const prefectures = [
+    { id: 1, name: "北海道", roman: "hokkaido" },
+    { id: 2, name: "青森", roman: "aomori" },
+    { id: 3, name: "岩手", roman: "iwate" },
+    { id: 4, name: "宮城", roman: "miyagi" },
+    { id: 5, name: "秋田", roman: "akita" },
+    { id: 6, name: "山形", roman: "yamagata" },
+    { id: 7, name: "福島", roman: "fukushima" },
+    { id: 8, name: "茨城", roman: "ibaraki" },
+    { id: 9, name: "栃木", roman: "tochigi" },
+    { id: 10, name: "群馬", roman: "gunma" },
+    { id: 11, name: "埼玉", roman: "saitama" },
+    { id: 12, name: "千葉", roman: "chiba" },
+    { id: 13, name: "東京", roman: "tokyo" },
+    { id: 14, name: "神奈川", roman: "kanagawa" },
+    { id: 15, name: "新潟", roman: "niigata" },
+    { id: 16, name: "富山", roman: "toyama" },
+    { id: 17, name: "石川", roman: "ishikawa" },
+    { id: 18, name: "福井", roman: "fukui" },
+    { id: 19, name: "山梨", roman: "yamanashi" },
+    { id: 20, name: "長野", roman: "nagano" },
+    { id: 21, name: "岐阜", roman: "gifu" },
+    { id: 22, name: "静岡", roman: "shizuoka" },
+    { id: 23, name: "愛知", roman: "aichi" },
+    { id: 24, name: "三重", roman: "mie" },
+    { id: 25, name: "滋賀", roman: "shiga" },
+    { id: 26, name: "京都", roman: "kyoto" },
+    { id: 27, name: "大阪", roman: "osaka" },
+    { id: 28, name: "兵庫", roman: "hyogo" },
+    { id: 29, name: "奈良", roman: "nara" },
+    { id: 30, name: "和歌山", roman: "wakayama" },
+    { id: 31, name: "鳥取", roman: "tottori" },
+    { id: 32, name: "島根", roman: "shimane" },
+    { id: 33, name: "岡山", roman: "okayama" },
+    { id: 34, name: "広島", roman: "hiroshima" },
+    { id: 35, name: "山口", roman: "yamaguchi" },
+    { id: 36, name: "徳島", roman: "tokushima" },
+    { id: 37, name: "香川", roman: "kagawa" },
+    { id: 38, name: "愛媛", roman: "ehime" },
+    { id: 39, name: "高知", roman: "kochi" },
+    { id: 40, name: "福岡", roman: "fukuoka" },
+    { id: 41, name: "佐賀", roman: "saga" },
+    { id: 42, name: "長崎", roman: "nagasaki" },
+    { id: 43, name: "熊本", roman: "kumamoto" },
+    { id: 44, name: "大分", roman: "oita" },
+    { id: 45, name: "宮崎", roman: "miyazaki" },
+    { id: 46, name: "鹿児島", roman: "kagoshima" },
+    { id: 47, name: "沖縄", roman: "okinawa" },
+  ];
+
+  const handleSelectChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const GetData = async () => {
+    try {
+      const response = await axios.get(`${url}/weather?city=${city}`);
+      // console.log("response", response.data.result_from_mysql);
+      // console.log("response1", response.data.result_from_mysql[0]);
+      setData(response.data.result_from_mysql);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  // アイコンコードからアイコン画像取得
+  const getIconUrl = (iconCode) => {
+    return `http://openweathermap.org/img/w/${iconCode}.png`;
+  };
+
+  //APIからデータ取得
+  // const GetApiData = async () => {
+  //   try {
+  //     const response = await axios.get(`${url}/weatherAPI?city=${city}`);
+  //     console.log("response", response.data.result_from_api);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "50vh",
+      }}
+    >
+      {/* <button onClick={GetApiData}>データインサート</button> */}
+      <div>天気予報</div>
+      <select value={city} onChange={handleSelectChange}>
+        <option>都道府県を選択してください</option>
+        {prefectures.map((prefecture) => (
+          <option key={prefecture.id} value={prefecture.roman}>
+            {prefecture.name}
+          </option>
+        ))}
+      </select>
+      <button onClick={GetData}>検索</button>
+      {data ? (
+        <div>
+          <p>天気: {data[0][0]}</p>{" "}
+          {data[0][1] && (
+            <img src={getIconUrl(data[0][1])} alt="Weather Icon" />
+          )}
+        </div>
+      ) : null}
     </div>
   );
 }
